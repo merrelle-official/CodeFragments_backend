21a5d9252f334a27392e444ed25765eb84d2b945
Merrell Ehrmantraut <95222587+merrelle-official@users.noreply.github.com>
2025-04-10 21:29:12 +0300

    fix user data response

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,6 +6,7 @@ import {
 
 export const register = async (req: Request, res: Response) => {
     try {
+        console.log(req.body);
         const { username, email, password } = req.body;
         const user = await registerUser(username, email, password);
         res.status(201).json({ message: "User registered successfully", user });
@@ -17,8 +18,8 @@ export const register = async (req: Request, res: Response) => {
 export const login = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body;
-        const { token, userId, username, userEmail } = await loginUser(email, password);
-        res.json({ token, userId, username, userEmail });
+        const { token, user: {userId, username, userEmail, userRole} } = await loginUser(email, password);
+        res.json({ token, user: { userId, username, userEmail, userRole } });
     } catch (err) {
         res.status(401).json({ error: err instanceof Error ? err.message : "An unknown error occurred" });
     }
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -9,12 +9,13 @@ const prisma = new PrismaClient();
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const registerUser = async (username: string, email: string, password: string) => {
+    
     const hashedPassword = await bcrypt.hash(password, 10);
     return await prisma.user.create({
         data: {
-            username,
             email,
-            password: hashedPassword
+            password: hashedPassword,
+            username,
         }
     });
 };
@@ -28,9 +29,18 @@ export const loginUser = async (email: string, password: string) => {
 
     const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "12h" });
 
-    return { token, userId: user.id, username: user.username, userEmail: user.email };
+    return { 
+        token, 
+        user: {
+            userId: user.id, 
+            username: user.username, 
+            userEmail: user.email, 
+            userRole: user.role
+        } };
 };
 
 export const verifyToken = (token: string) => {
     return jwt.verify(token, JWT_SECRET);
 };
+
+
